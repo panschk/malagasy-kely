7d2649d451cfab76efab65cbc2eb85b951d345a6
panschk <panschk@googlemail.com>
2017-11-05 21:02:49 +0100

    styling

--- js/mifidy_mianatra.js
+++ js/mifidy_mianatra.js
@@ -1,6 +1,10 @@
 var app = angular.module('select', []);
 app.controller('Main', ['$scope', function Main($scope) {
 	$scope.L = L;
+	$scope.M = M;
+	$scope.getPic = function(level, index) {
+		return M[level][index];
+	};
 }]);
 
 
